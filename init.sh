db4b3932ff1a50c809cbfcd503906f823f47fdd7
wxmuma <wxmuma@qq.com>
2026-05-17 06:59:31 +0800

    Update init.sh for Laravel 12
    - Remove joanhey/adapterman (Webman adapter, not needed for Laravel)
    - Add PHP 8.2+ version check before installation
    - Use --no-dev --optimize-autoloader for production install

--- init.sh
+++ init.sh
@@ -2,11 +2,13 @@
 
 rm -rf composer.phar
 wget https://github.com/composer/composer/releases/latest/download/composer.phar -O composer.phar
-php composer.phar install -vvv
+php composer.phar install --no-dev --optimize-autoloader
 
 php_main_version=$(php -v | head -n 1 | cut -d ' ' -f 2 | cut -d '.' -f 1)
-if [ $php_main_version -ge 8 ]; then
-    php composer.phar require joanhey/adapterman
+php_sub_version=$(php -v | head -n 1 | cut -d ' ' -f 2 | cut -d '.' -f 2)
+if [ $php_main_version -lt 8 ] || ([ $php_main_version -eq 8 ] && [ $php_sub_version -lt 2 ]); then
+    echo "Error: PHP 8.2+ is required. Current version: $(php -v | head -n 1 | cut -d ' ' -f 2)"
+    exit 1
 fi
 
 php artisan v2board:install
